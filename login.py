c94c53ca3193b6d34c6aeaf54f90391a869c4884
Trisna22 <30829878+Trisna22@users.noreply.github.com>
2019-12-06 13:35:13 +0100

    update login.py

--- login.py
+++ login.py
@@ -6,12 +6,14 @@
 
 import mysql.connector as mariaDB
 import urllib.parse as urlparse
+import os
+import subprocess
 
 # Verstuurt content van HTML fout login.
 def wrong_login(environ, start_response):
 	status = '200 OK'
 
-	# Open file with wrong password.
+	# Open file with wrong login.
 	lines = []
 	file = open('/var/www/FYS/encrypted/wrong_login.html', 'r')
 	for i in file:
@@ -24,22 +26,104 @@ def wrong_login(environ, start_response):
 	start_response(status, response_header)
 	return [bytes(html, 'utf-8')]
 
-
-# Verstuurt content van HTML goed login.
-def correct_login(environ, start_response):
+# Verstuurt content van HTML ticket al ingelogd.
+def ticket_already_used(environ, start_response):
 	status = '200 OK'
 
-	html = '<html>'
-	html += '<body>Ingelogd!</body>'
-	html += '</html>'
+	# Open file with ticket already logged in.
+	lines = []
+	file = open('/var/www/FYS/encrypted/ticket_already_used.html')
+	for i in file:
+		lines.append(str(i))
+	file.close()
+
+	html = '\n'.join(lines)
 
 	response_header = [('Content-type', 'text/html')]
 	start_response(status, response_header)
 	return [bytes(html, 'utf-8')]
 
+# Update de databank met tickets die al ingelogd zijn.
+def updateLoggedInDatabase(IP, MAC, ticketNumber, seatNumber, lastName):
+
+	connection = mariaDB.connect(host='127.0.0.1', user='root', passwd='IC106_2', db='CaptivePortalDB')
+	cursor = connection.cursor()
+
+	# Nu we weten dat het ticketnummer nog niet ingelogd is,
+	# kunnen we de gegevens in de databank inserten.
+	insertQuery = "INSERT INTO LoggedIn VALUES (\'" + IP + "\', \'" + MAC + "\', \'"
+	insertQuery += ticketNumber + "\', \'" + seatNumber + "\', \'" + lastName + "\');"
+	cursor.execute(insertQuery)
+
+	connection.commit()
+
+	# Als we klaar zijn met de database, moeten we de IPtables ook configureren,
+	# dat het IP address internet mag gebruiken, en de PREROUTING uitgezet.
+	redirect_commands = ['sudo', 'iptables', '-t', 'nat', '-D', 'PREROUTING', '-s', IP,
+		'-p', 'tcp', '--dport', '80', '-j', 'DNAT', '--to-destination', '192.168.22.1:80']
+	subprocess.Popen(redirect_commands, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	redirect_commands2 = ['sudo', 'iptables', '-t', 'nat', '-D', 'PREROUTING', '-s', IP,
+		'-p', 'tcp', '--dport', '443', '-j', 'DNAT', '--to-destination', '192.168.22.1:443']
+	subprocess.Popen(redirect_commands2, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+	command = ['sudo', 'iptables', '-t', 'nat', '-A', 'POSTROUTING', '--source', IP, 
+		'-o', 'eth0', '-j', 'MASQUERADE']
+	p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+# Verstuurt content van HTML goed login.
+def correct_login(environ, start_response, ticketNumber, seatNumber, IP, MAC):
+
+	connection = mariaDB.connect(host='127.0.0.1', user='root', passwd='IC106_2', db='CaptivePortalDB')
+	cursor = connection.cursor()
+
+	# Verkrijg de firstName van de databank ovor onze html content.
+	cursor.execute('SELECT firstName FROM Passengers WHERE ticketNumber = ' + ticketNumber + ';')
+	result = cursor.fetchall()
+	firstName = result[0][0].encode('ascii').decode('utf-8')
+
+	# Verkrijg nog de laatste vereiste variabele lastName.
+	cursor.execute('SELECT lastName FROM Passengers WHERE ticketNumber = ' + ticketNumber + ';')
+	result = cursor.fetchall()
+	lastName = result[0][0].encode('ascii').decode('utf-8')
+
+	# Update the LoggedIn databank.
+	updateLoggedInDatabase(IP, MAC, ticketNumber, seatNumber, lastName)
+
+	# HTML content.
+	status = '200 OK'
+	html = '<html>'
+	html += '<body>'
+	html += '<h1>You have succesfully logged in!</h1>'
+	html += '<br><br><br>'
+	html += 'Dear {{firstName}} {{lastName}}, <br>'
+	html += 'MAC: {{MAC}}<br>'
+	html += 'IP Address: {{IP}}<br>'
+	html += '</body>'
+	html += '</html>'
+
+
+	html = html.replace('{{firstName}}', firstName)
+	html = html.replace('{{lastName}}', lastName)
+	html = html.replace('{{MAC}}', MAC)
+	html = html.replace('{{IP}}', IP)
+
+	response_header = [('Content-type', 'text/html')]
+	start_response(status, response_header)
+	return [bytes(html, 'utf-8')]
+
+# Verkrijg het MAC address van de bijbehorende IP address.
+def GetMacFromIP(IP):
+	MAC = os.popen("arp -n | grep \"" + IP + "\" | awk \'{print $3}\'").read()
+	return MAC[:-1]
 
 # Checkt of het apparaat dat op de site komt al is ingelogd.
-def checkIfDeviceLoggedIn(IP, MAC):
+def checkIfDeviceLoggedIn(environ):
+
+	# Verkrijg de benodigde MAC en IP adres.
+	IP = environ['REMOTE_ADDR']
+	MAC = GetMacFromIP(IP)
+
 	connection = mariaDB.connect(host='127.0.0.1', user='root', passwd='IC106_2', db='CaptivePortalDB')
 	cursor = connection.cursor()
 
@@ -48,6 +132,17 @@ def checkIfDeviceLoggedIn(IP, MAC):
 		IP + '\' OR macAddress = \'' + MAC + '\');')
 
 	# Resultaat van SQL Query.
+	result = cursor.fetchall()
+	return result[0][0]
+
+# Checkt of het ticketnummer al ingelogd is.
+def checkIfTicketLoggedIn(ticketNumber, seatNumber):
+	connection = mariaDB.connect(host='127.0.0.1', user='root', passwd='IC106_2', db='CaptivePortalDB')
+	cursor = connection.cursor()
+
+	# SQL Query om te checken.
+	cursor.execute('SELECT EXISTS ( SELECT * FROM LoggedIn WHERE ticketNumber = \'' +
+		ticketNumber + '\' OR seatNumber = \'' + seatNumber + '\');')
 	result = cursor.fetchall()
 	return result[0][0]
 
@@ -87,12 +182,6 @@ def checkStringValue(string):
 # Checkt of de ticketnummer met de stoelnummer correct zijn.
 def checkCredentials(ticketNumber, seatNumber):
 
-	# Kijken of de externe variabelen veilig zijn.
-	if checkStringValue(ticketNumber) == False:
-		return False
-	if checkStringValue(seatNumber) == False:
-		return False
-
 	# Nu we weten dat de variabelen veilig zijn,
 	# kunnen we kijken of het ticketnummer en stoelnummer kloppen.
 	connection = mariaDB.connect(host='127.0.0.1', user='root', passwd='IC106_2', db='CaptivePortalDB')
@@ -106,17 +195,31 @@ def checkCredentials(ticketNumber, seatNumber):
 	return result[0][0]
 
 
-# Het inloggen van de gebruiker.
+# Het inloggen van de gebruiker. (POST request)
 def doLogin(environ, start_response):
 
+	# Verkrijg de benodigde MAC en IP adres.
+	IP = environ['REMOTE_ADDR']
+	MAC = GetMacFromIP(IP)
+
+	# Parse de POST variabelen van onze form.
 	s = environ['wsgi.input'].read().decode()
 	params = urlparse.parse_qs(s)
+	ticketNumber = params.get('TICKETNUMBER', [''])[0]
+	seatNumber = params.get('SEATNUMBER', [''])[0]
 
-	TICKETNUMBER = params.get('TICKETNUMBER', [''])[0]
-	SEATNUMBER = params.get('SEATNUMBER', [''])[0]
+	# Kijken of de externe variabelen veilig zijn.
+	if checkStringValue(ticketNumber) == False:
+		return wrong_login(environ, start_response)
+	if checkStringValue(seatNumber) == False:
+		return wrong_login(environ, start_response)
+
+	# Checken of de ticket en stoelnummer al gebruikt zijn om in te loggen.
+	if checkIfTicketLoggedIn(ticketNumber, seatNumber) == True:
+		return ticket_already_used(environ, start_response)
 
 	# Login check.
-	if checkCredentials(TICKETNUMBER, SEATNUMBER) == False:
+	if checkCredentials(ticketNumber, seatNumber) == False:
 		return wrong_login(environ, start_response)
 	else:
-		return correct_login(environ, start_response)
+		return correct_login(environ, start_response, ticketNumber, seatNumber, IP, MAC)
